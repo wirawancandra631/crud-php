520f679930db4653b6c31b04a23a47db041391a6
wirawancandra631 <86940695+wirawancandra631@users.noreply.github.com>
2022-04-10 11:41:34 +0700

    Update save.php

--- save.php
+++ save.php
@@ -9,7 +9,7 @@ $kontak=htmlspecialchars($_POST['kontak']);
 $sampul=$_FILES['sampul'];
 $sampulName=$sampul["name"];
 $sampulFile=$sampul["tmp_name"];
-move_uploaded_file($sampulFile,"img/".$sampulName);
+move_uploaded_file($sampulFile,"styld/".$sampulName);
 $query="
 INSERT INTO tbl_siswa (nama,kontak,sampul)
 VALUES('$nama','$kontak','$sampulName')
